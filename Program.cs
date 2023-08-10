3baf4dfa6f9aa3fa9def0426489c154923152763
David Waltenburg <david@waltenburg.dk>
2023-08-10 13:15:40 +0200

    Overflødig variabel fjernet

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace Rap_Finands
         public static string reginummer = "4242";
         public static string datafil = "bank.json"; //Her ligger alt data i
         public static List<Konto> konti;
-        public static float belob;
+        //Overflødig variabel fjernet
         static void Main(string[] args)
         {
             Console.WriteLine("Henter alt kontodata");
